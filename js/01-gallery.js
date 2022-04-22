fdcfcca69e0799ad572d262eb49faaf6413d7e1a
Max_Hrytsenia <95626499+MaxymH@users.noreply.github.com>
2022-04-22 17:48:25 +0200

    п

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryNav = document.querySelector(".gallery");
+
+const  createGallery = galleryItems => {
+  return galleryItems
+      .map(({ preview, original, description }) => {
+        
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+const imageGallery = createGallery(galleryItems);
+galleryNav.innerHTML = imageGallery;
+
+// const  instance  =  basicLightbox . create ( ` 
+// 	<h1>Динамічний вміст</h1> 
+// 	<p>Ви можете встановити вміст лайтбоксу за допомогою JS.</p> 
+// ` )
+// instance.show()
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+// Change code below this line
+const galleryNav = document.querySelector(".gallery");
+
+const  createGallery = galleryItems => {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+  <a class="gallery__item" href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    alt="${description}"
+    />
+  </a>`;
+    })
+    .join("");
+}
+
+const imageGallery = createGallery(galleryItems);
+galleryNav.innerHTML = imageGallery;
+
+let animationAlt = new SimpleLightbox(".gallery a", {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
