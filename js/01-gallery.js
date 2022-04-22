309f0ae112481a1260bd5352ba201deeb148cb18
Max_Hrytsenia <95626499+MaxymH@users.noreply.github.com>
2022-04-22 19:33:18 +0200

    commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,13 +3,14 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const galleryNav = document.querySelector(".gallery");
+const nav = document.querySelector('.gallery')
 
 const  createGallery = galleryItems => {
   return galleryItems
       .map(({ preview, original, description }) => {
         
       return `<div class="gallery__item">
-  <a class="gallery__link" href="${original}">
+  <a class="gallery__link" >
     <img
     class="gallery__image"
     src="${preview}"
@@ -22,11 +23,21 @@ const  createGallery = galleryItems => {
     .join("");
 }
 
+const showModalImg = (e) => {
+  const srcOriginal = e.target.dataset.source
+  
+  if (!e.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+
+  const instance = basicLightbox.create(`
+    <img src='${srcOriginal}' width="800" height="600">
+`) 
+instance.show()
+}
+nav.addEventListener('click', showModalImg)
+
 const imageGallery = createGallery(galleryItems);
 galleryNav.innerHTML = imageGallery;
 
-// const  instance  =  basicLightbox . create ( ` 
-// 	<h1>Динамічний вміст</h1> 
-// 	<p>Ви можете встановити вміст лайтбоксу за допомогою JS.</p> 
-// ` )
-// instance.show()
